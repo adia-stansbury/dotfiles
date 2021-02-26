1eeb1b19fb68c04f994381c665ad1301db681052
Adia Porter <adia-stansbury@users.noreply.github.com>
2021-02-25 19:38:42 -0500

    Delete sources.sh

--- .dotfiles/work/sources.sh
+++
@@ -1,3 +0,0 @@
-source $HOME/engineering_tools/development/shell-functions.sh
-source $HOME/engineering_tools/maintain_env/maintain-env.sh
-source $HOME/engineering_tools/maintain_env/sample_custom_scripts.sh
